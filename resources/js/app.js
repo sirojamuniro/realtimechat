1ec614895f85d90f9c775489e6628dafd83ca7a7
Siroja Muniro <sgifwd2@gmail.com>
2021-02-26 15:57:30 +0700

    vue chat

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
 require('./bootstrap');
+
+window.Vue = require('vue').default;
+import Vue from 'vue'
+import VuechatScroll from 'vue-chat-scroll'
+Vue.use(VuechatScroll)
+
+Vue.component('message', require('./components/message.vue').default);
+
+/**
+ * Next, we will create a fresh Vue application instance and attach it to
+ * the page. Then, you may begin adding components to this application
+ * or customize the JavaScript scaffolding to fit your unique needs.
+ */
+
+const app = new Vue({
+    el: '#app',
+    data: {
+        message: '',
+        chat: {
+            message: []
+        }
+    },
+    methods: {
+        send() {
+            if (this.message.length != 0) {
+                this.chat.message.push(this.message);
+                this.message = ''
+            }
+        }
+    }
+});
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+ROute::get('chat',function(){
+    return view('chat');
+});
